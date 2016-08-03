2c52dab3242dd8decf3d381a872fa4d16d7ed3b9
Rafael Eyng <rafaeleyng@gmail.com>
2016-08-03 00:25:38 -0300

    add tests for concat mode

--- test/test.js
+++ test/test.js
@@ -5,13 +5,46 @@ const states = [
   {
     name: 'start',
     properties: {
-      show: 'someButton',
+      show: {
+        some: true,
+      },
+      text: {
+        title: 'Start title',
+      },
     },
   },
   {
     name: 'second',
     properties: {
-      show: ['otherButton'],
+      show: ['some', 'other', 'another'],
+      text: {
+        subtitle: 'Second subtitle',
+      },
+    },
+  },
+  {
+    name: 'third',
+    properties: {
+      show: ['other'],
+      text: {
+        title: 'Third title',
+        subtitle: 'Third subtitle',
+      },
+    },
+  },
+  {
+    name: 'fourth',
+    properties: {
+      show: 'some',
+      text: {
+        comment: 'no title or subtitle on fourth',
+      },
+    },
+  },
+  {
+    name: 'fifth',
+    properties: {
+      show: ['other', 'another'],
     },
   },
 ]
@@ -22,11 +55,77 @@ test.beforeEach(() => {
   macchina = new Macchina(states)
 })
 
+/*
+  state changes
+*/
 test('initial state is "start"', t => {
   t.is(macchina.state(), 'start')
 })
 
-test('transition to other state changes the state accordingly', t => {
+test('immediateTransition changes state correctly', t => {
   macchina.immediateTransition('second')
   t.is(macchina.state(), 'second')
+})
+
+/*
+  properties
+*/
+test('boolean properties are set correctly in concat mode', t => {
+  t.is(macchina.properties().showSome, true)
+  t.is(macchina.properties().showOther, undefined)
+  t.is(macchina.properties().showAnother, undefined)
+
+  macchina.immediateTransition('second')
+
+  t.is(macchina.properties().showSome, true)
+  t.is(macchina.properties().showOther, true)
+  t.is(macchina.properties().showAnother, true)
+
+  macchina.immediateTransition('third')
+
+  t.is(macchina.properties().showSome, undefined)
+  t.is(macchina.properties().showOther, true)
+  t.is(macchina.properties().showAnother, undefined)
+
+  macchina.immediateTransition('fourth')
+
+  t.is(macchina.properties().showSome, true)
+  t.is(macchina.properties().showOther, undefined)
+  t.is(macchina.properties().showAnother, undefined)
+
+  macchina.immediateTransition('fifth')
+
+  t.is(macchina.properties().showSome, undefined)
+  t.is(macchina.properties().showOther, true)
+  t.is(macchina.properties().showAnother, true)
+})
+
+test('regular properties are set correctly in concat mode', t => {
+  t.is(macchina.properties().textTitle, 'Start title')
+  t.is(macchina.properties().textSubtitle, undefined)
+  t.is(macchina.properties().textComment, undefined)
+
+  macchina.immediateTransition('second')
+
+  t.is(macchina.properties().textTitle, undefined)
+  t.is(macchina.properties().textSubtitle, 'Second subtitle')
+  t.is(macchina.properties().textComment, undefined)
+
+  macchina.immediateTransition('third')
+
+  t.is(macchina.properties().textTitle, 'Third title')
+  t.is(macchina.properties().textSubtitle, 'Third subtitle')
+  t.is(macchina.properties().textComment, undefined)
+
+  macchina.immediateTransition('fourth')
+
+  t.is(macchina.properties().textTitle, undefined)
+  t.is(macchina.properties().textSubtitle, undefined)
+  t.is(macchina.properties().textComment, 'no title or subtitle on fourth')
+
+  macchina.immediateTransition('fifth')
+
+  t.is(macchina.properties().textTitle, undefined)
+  t.is(macchina.properties().textSubtitle, undefined)
+  t.is(macchina.properties().textComment, undefined)
 })
